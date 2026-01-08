c04184a49e4543210253e56b74646f12f2f1a97f
Rodrigo Costa <rodrigocosta.rsc2878@gmail.com>
2026-01-08 09:20:08 +0000

    fstream c++

---
+++ 11IF/PROG/files/f_treino.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+struct {
+	string nome;
+	int idade{};
+	int numero{};
+}alu;
+void inserir(){
+	fstream file("alunos.txt",fstream::app);
+	cout<<"nome: ";
+	cin.ignore();
+	getline(cin,alu.nome);
+	cout<<"idade: ";
+	cin>>alu.idade;
+	cout<<"numero: ";
+	cin>>alu.numero;
+	file<<alu.nome<<" "<<alu.idade<<" "<<alu.numero<<endl;
+	file.close();
+}
+void listar(){
+	string linha;
+	ifstream file("alunos.txt");
+	while (getline(file,linha)){
+		cout<<linha<<endl;
+	}
+	file.close();
+	cout<<endl;
+}
+using namespace std;
+int main(){
+	int op=-1;
+	while(op!=0){
+		cout<<"MENU"<<endl<<"1.INSERIR"<<endl<<"2.LISTAR"<<endl<<"0. FIM"<<endl;
+		cin>>op;
+		switch (op){
+			case 1:
+				inserir();
+				break;
+			case 2:
+				listar();
+				break;
+			default:break;
+		}
+	}
+	return 0;
+}
